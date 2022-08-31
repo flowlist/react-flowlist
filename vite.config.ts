23bca11c69375875c3ccd8549ad133a1eaa7ae6b
falstack <icesilt@outlook.com>
2022-08-31 08:59:21 +0800

    update dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+        insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: './src/main.tsx',
